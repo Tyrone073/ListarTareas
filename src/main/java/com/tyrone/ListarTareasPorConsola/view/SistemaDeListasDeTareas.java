9fe86db69ba0757a5011fdd87c165e91f5a9cb80
Tyrone Pilay <tpilay3588@utm.edu.ec>
2025-05-20 18:48:08 -0500

    manejando error de la linea de comando tipo int

--- src/main/java/com/tyrone/ListarTareasPorConsola/view/SistemaDeListasDeTareas.java
+++ src/main/java/com/tyrone/ListarTareasPorConsola/view/SistemaDeListasDeTareas.java
@@ -3,6 +3,7 @@ package main.java.com.tyrone.ListarTareasPorConsola.view;
 import main.java.com.tyrone.ListarTareasPorConsola.controller.ListaTareas;
 import main.java.com.tyrone.ListarTareasPorConsola.model.Tarea;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class SistemaDeListasDeTareas {
@@ -11,9 +12,8 @@ public class SistemaDeListasDeTareas {
     private Scanner scanner= new Scanner(System.in);
 
     public void ejecutar() {
-        int opcion;
+        int opcion = 0;
         Long idTarea;
-
 
         do {
             System.out.println("\n1. Agregar tarea");
@@ -22,42 +22,49 @@ public class SistemaDeListasDeTareas {
             System.out.println("4. Mostrar tareas");
             System.out.println("0. Salir");
             System.out.print("Elige una opción: ");
-            opcion = scanner.nextInt();
-            scanner.nextLine(); // limpiar buffer
+
+            try {
+                opcion = scanner.nextInt();
+                scanner.nextLine(); // limpiar buffer
 
 
-            if (opcion == 1) {
-                System.out.print("Nombre de la tarea: ");
-                String nombre = scanner.nextLine();
-                System.out.print("Descripcion: ");
-                String descripcion = scanner.nextLine();
-                Tarea nuevatarea = new Tarea(nombre, descripcion);
-                listaTareas.agregarTarea(nuevatarea);
+                if (opcion == 1) {
+                    System.out.print("Nombre de la tarea: ");
+                    String nombre = scanner.nextLine();
+                    System.out.print("Descripcion: ");
+                    String descripcion = scanner.nextLine();
+                    Tarea nuevatarea = new Tarea(nombre, descripcion);
+                    listaTareas.agregarTarea(nuevatarea);
 
-            } else if (opcion == 2) {
-                System.out.print("Seleccione que tarea desea macar como completada:");
-                listaTareas.mostrarTareas();
-                if (listaTareas.isEmpty()){
-                    System.out.println("Agrege una tarea");
-                }else {
-                    System.out.println("Escriba el indice de la tarea: ");
-                    idTarea = scanner.nextLong();
-                    listaTareas.actualizarEstado(idTarea);
-                    System.out.println("Se acaba de marcar como completa la tarea: \n" + listaTareas.buscarTareaPorId(idTarea));
+                } else if (opcion == 2) {
+                    System.out.print("Seleccione que tarea desea macar como completada:");
+                    listaTareas.mostrarTareas();
+                    if (listaTareas.isEmpty()) {
+                        System.out.println("Agrege una tarea");
+                    } else {
+                        System.out.println("Escriba el indice de la tarea: ");
+                        idTarea = scanner.nextLong();
+                        listaTareas.actualizarEstado(idTarea);
+                        System.out.println("Se acaba de marcar como completa la tarea: \n" + listaTareas.buscarTareaPorId(idTarea));
+                    }
+
+                } else if (opcion == 3) {
+                    System.out.print("Seleccione que tarea desea eliminar: ");
+                    listaTareas.mostrarTareas();
+                    if (listaTareas.isEmpty()) {
+                        System.out.println("Agrege una tarea");
+                    } else {
+                        System.out.println("Escriba el indice de la tarea");
+                        idTarea = scanner.nextLong();
+                        listaTareas.borrarTarea(idTarea);
+                    }
+                } else if (opcion == 4) {
+                    listaTareas.mostrarTareas();
                 }
 
-            }else if (opcion == 3) {
-                System.out.print("Seleccione que tarea desea eliminar: ");
-                listaTareas.mostrarTareas();
-                if (listaTareas.isEmpty()){
-                    System.out.println("Agrege una tarea");
-                }else {
-                    System.out.println("Escriba el indice de la tarea");
-                    idTarea = scanner.nextLong();
-                    listaTareas.borrarTarea(idTarea);
-                }
-            }else if (opcion == 4) {
-                listaTareas.mostrarTareas();
+            }catch (InputMismatchException e) {
+                System.out.println("❌ Entrada inválida. Usa números del menú.");
+                scanner.nextLine(); // limpiar buffer
             }
         }while (opcion != 0);
     }
